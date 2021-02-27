08eb45e371250432054015ff1c90a86dc2ccaae1
muhhylmi <hylmi.muh@gmail.com>
2021-02-27 15:26:41 +0700

    render total item on cart icon update item by sending json data update item via button addto cart update item via arrow icon

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,5 +1,5 @@
 var updateBtns = document.querySelectorAll(".update-cart");
-console.log(updateBtns);
+
 updateBtns.forEach((updateBtn) => {
   updateBtn.addEventListener("click", function () {
     var productId = this.dataset.product;
@@ -11,7 +11,28 @@ updateBtns.forEach((updateBtn) => {
     if (user == "AnonymousUser") {
       console.log("User is not authenticated");
     } else {
-      console.log("User Logined sending data");
+      updateUserOrder(productId, action);
     }
   });
 });
+
+function updateUserOrder(productId, action) {
+  console.log("User is authenticated, sending data...");
+
+  var url = "/update_item/";
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrftoken,
+    },
+    body: JSON.stringify({ productId: productId, action: action }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      location.reload();
+    });
+}
