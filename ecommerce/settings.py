1982555651e61ecdcffa89d0c7da974c3f1045e7
muhhylmi <hylmi.muh@gmail.com>
2021-03-05 11:06:02 +0700

    initial to deploy

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'a#a#cc3((c+3s--kvm5k&umz!4onr67!f3@%s1ra*akt$0%&ov'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['toku-ku.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
